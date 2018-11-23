6433f36221d4463fef6ebfda9ee0a57324132475
Nathan K. Burr <35701804+nathanburr@users.noreply.github.com>
2018-11-23 16:51:26 -0600

    Library temporarily populated with test strings, will load PlayerFragment on click

--- app/src/main/java/com/coderunners/spoofify/LibraryFragment.java
+++ app/src/main/java/com/coderunners/spoofify/LibraryFragment.java
@@ -1,18 +1,26 @@
 package com.coderunners.spoofify;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LibraryFragment extends Fragment {
-
+public class LibraryFragment extends Fragment
+{
+    // Temporary test values
+    private String items[] = new String[] {"Apple", "Orange", "Banana", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"};
+    private OnFragmentInteractionListener fListener;
 
     public LibraryFragment() {
         // Required empty public constructor
@@ -20,10 +28,39 @@ public class LibraryFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false);
+        View view = inflater.inflate(R.layout.fragment_library, container, false);
+
+        ListView listView = view.findViewById(R.id.songlist);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+
+        listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                //Toast.makeText(getActivity(), items[position], Toast.LENGTH_SHORT).show();
+                // Loads the PlayerFragment view
+                fListener = (OnFragmentInteractionListener) getActivity();
+                fListener.changeFragment(3);
+            }
+        });
+
+
+
+        return view;
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+        fListener = null;
     }
 
 }

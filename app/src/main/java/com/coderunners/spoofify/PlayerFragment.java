13c6d966c9db9332899b56e407f32c2b57628eaa
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2018-11-23 21:44:22 -0600

    connected music player to the view

--- app/src/main/java/com/coderunners/spoofify/PlayerFragment.java
+++ app/src/main/java/com/coderunners/spoofify/PlayerFragment.java
@@ -1,7 +1,10 @@
 package com.coderunners.spoofify;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,19 +14,78 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PlayerFragment extends Fragment {
-
+public class PlayerFragment extends Fragment
+{
+    private FloatingActionButton play;
+    private FloatingActionButton pause;
+    private FloatingActionButton previous;
+    private FloatingActionButton next;
+    private MediaPlayer mp;
+    private int resumeFlag=0;
+    private int resumePosition;
 
     public PlayerFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_player, container, false);
+        play = (FloatingActionButton) view.findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    mp = MediaPlayer.create(getActivity(),R.raw.jinglebells);
+                    mp.start();
+            }
+        });
+
+        pause = (FloatingActionButton) view.findViewById(R.id.pause);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if (mp.isPlaying() && resumeFlag == 0)
+                {
+                    mp.pause();
+                    resumePosition = mp.getCurrentPosition();
+                }
+                if((!mp.isPlaying()) && resumeFlag == 1)
+                {
+                    mp.seekTo(resumePosition);
+                    mp.start();
+                }
+                if(resumeFlag == 0) resumeFlag = 1;
+                else resumeFlag = 0;
+            }
+        });
+
+        previous = (FloatingActionButton) view.findViewById(R.id.previous);
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mp.isPlaying())
+                    mp.stop();
+                mp = MediaPlayer.create(getActivity(),R.raw.chainsmokers);
+                mp.start();
+            }
+        });
+
+        next = (FloatingActionButton) view.findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mp.isPlaying())
+                    mp.stop();
+                mp = MediaPlayer.create(getActivity(),R.raw.deckthehalls);
+                mp.start();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player, container, false);
+        return view;
     }
 
 }

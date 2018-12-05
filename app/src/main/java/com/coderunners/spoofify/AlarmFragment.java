da2dc7a7a1bee0245ec9e19539bde640cb1e3f7f
Juan Diaz Sada <jdsada30@gmail.com>
2018-12-04 19:18:42 -0600

    Finished Alarm View

--- app/src/main/java/com/coderunners/spoofify/AlarmFragment.java
+++ app/src/main/java/com/coderunners/spoofify/AlarmFragment.java
@@ -1,11 +1,17 @@
 package com.coderunners.spoofify;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 
 /**
@@ -13,7 +19,12 @@ import android.view.ViewGroup;
  */
 public class AlarmFragment extends Fragment {
 
-
+    TimePicker alarmTime;
+    Button setAlarm;
+    TextView tvcurrentalarm;
+    int minute;
+    int hour;
+    String alarmtext;
     public AlarmFragment() {
         // Required empty public constructor
     }
@@ -22,8 +33,28 @@ public class AlarmFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_alarm, container, false);
+        alarmTime = rootView.findViewById(R.id.timePicker);
+        setAlarm = rootView.findViewById(R.id.alarmSetButton);
+
+        tvcurrentalarm = rootView.findViewById(R.id.currentAlarmText);
+
+        setAlarm.setOnClickListener(new View.OnClickListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+               hour = alarmTime.getHour();
+               minute = alarmTime.getMinute();
+               alarmtext  = "Current Alarm: "+hour+":" + minute;
+                tvcurrentalarm.setText(alarmtext);
+
+            }
+        });
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false);
+        return rootView;
     }
 
 }

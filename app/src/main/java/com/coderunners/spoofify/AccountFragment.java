8f335f8c40a7fb855049d4e156fb282d7b0a1076
Nathan K. Burr <35701804+nathanburr@users.noreply.github.com>
2018-12-05 08:53:29 -0600

    populated account view

--- app/src/main/java/com/coderunners/spoofify/AccountFragment.java
+++ app/src/main/java/com/coderunners/spoofify/AccountFragment.java
@@ -6,13 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AccountFragment extends Fragment {
-
+public class AccountFragment extends Fragment
+{
+    private String options[] = {"Update Email", "Update Password", "Update Personal Details", "Contact Us", "Delete Account"};
+    private ListView listView;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -21,9 +27,36 @@ public class AccountFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        ListView listView = view.findViewById(R.id._account_list);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, options);
+
+        listView.setAdapter(listViewAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                if(position == 4)
+                {
+                    Toast.makeText(getActivity(), "I'm Sorry, Dave.  I'm Afraid I Can't Do That.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getActivity(), "Coming Soon in a Future Update", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        return view;
     }
 
 }

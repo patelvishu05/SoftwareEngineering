beb3ea7bf788a392de1b165016f94b69ac3a749b
Juan Diaz Sada <jdsada30@gmail.com>
2018-12-04 23:49:49 -0600

    finished account fragment, employees can now post into feed

--- app/src/main/java/com/coderunners/spoofify/AccountFragment.java
+++ app/src/main/java/com/coderunners/spoofify/AccountFragment.java
@@ -3,9 +3,13 @@ package com.coderunners.spoofify;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Objects;
 
 
 /**
@@ -13,17 +17,30 @@ import android.view.ViewGroup;
  */
 public class AccountFragment extends Fragment {
 
+    Button makeNewsPost;
 
     public AccountFragment() {
         // Required empty public constructor
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_account, container, false);
+        makeNewsPost= rootView.findViewById(R.id.postNewsButton);
+        makeNewsPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new PostNewsFragment();
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.main_frame, fragment);
+                fragmentTransaction.commit();
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        return rootView;
     }
 
 }
